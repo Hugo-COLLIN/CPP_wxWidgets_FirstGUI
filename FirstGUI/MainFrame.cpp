e17d688988c302f568cefbfe9f955e67ffa05c3a
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-05 18:39:28 +0200

    Controls - https://youtu.be/_2FMt_0wVMM

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -1,6 +1,38 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
+#include <wx/spinctrl.h>
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
+	wxPanel* panel = new wxPanel(this);
 
+	wxButton* button = new wxButton(panel, wxID_ANY, "Mybutton", wxPoint(150, 50), wxSize(100, 35));
+
+	wxCheckBox* checkbox = new wxCheckBox(panel, wxID_ANY, "My Checkbox", wxPoint(550, 55));
+
+	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, "Static text, not editable", wxPoint(120, 155));
+
+	wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, "editable text", wxPoint(500, 145), wxSize(200, -1));
+
+	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1));
+
+	wxGauge* progressBar = new wxGauge(panel, wxID_ANY, 100, wxPoint(500, 225), wxSize(200, -1));
+	progressBar->SetValue(50);
+
+
+	wxArrayString choices;
+	choices.Add("Item A");
+	choices.Add("Item B");
+	choices.Add("Item C");
+
+	wxChoice* choice = new wxChoice(panel, wxID_ANY, wxPoint(150, 375), wxSize(100, -1), choices);
+	choice->Select(0);
+
+
+	wxSpinCtrl* spinCtrl = new wxSpinCtrl(panel, wxID_ANY, "", wxPoint(550, 375), wxSize(100, -1));
+
+	wxListBox* listBox = new wxListBox(panel, wxID_ANY, wxPoint(150, 475), wxSize(100, -1), choices); //choices updatable during runtime
+
+	wxRadioBox* radioBox = new wxRadioBox(panel, wxID_ANY, "radioBox", wxPoint(485, 475), wxDefaultSize, choices);
+
+	//wxWidgets automatically deallocate memory for these controls!
 }
