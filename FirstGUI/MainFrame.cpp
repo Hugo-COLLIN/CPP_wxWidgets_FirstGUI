a7c2aa66e1b48c53cd0eb7eccc7d2c6fec59acb8
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-05 21:42:14 +0200

    Static Events - https://youtu.be/MRTrN0THsaA

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -1,43 +1,43 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
-#include <wx/spinctrl.h>
+
+// Rules to define custom IDs:
+// 1. Positive
+// 2. Not 0 or 1
+// 3. Not between wxID_LOWEST (4999) and wxID_HIGHEST (5999)
+
+enum IDs {
+	BUTTON_ID = 2,
+	SLIDER_ID = 3,
+	TEXT_ID = 4
+};
+
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+	EVT_BUTTON(BUTTON_ID, MainFrame::OnButtonClicked)
+	EVT_SLIDER(SLIDER_ID, MainFrame::OnSliderChanged)
+	EVT_TEXT(TEXT_ID, MainFrame::OnTextChanged)
+wxEND_EVENT_TABLE()
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 
-	wxButton* button = new wxButton(panel, wxID_ANY, "Mybutton", wxPoint(150, 50), wxSize(100, 35), wxBU_LEFT); // here, last parameter is a style
+	wxButton* button = new wxButton(panel, BUTTON_ID, "My button", wxPoint(50, 50), wxSize(100, 50));
+	wxSlider* slider = new wxSlider(panel, SLIDER_ID, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
+	wxTextCtrl* text = new wxTextCtrl(panel, TEXT_ID, "", wxPoint(300, 375), wxSize(200, -1));
 
-	wxCheckBox* checkbox = new wxCheckBox(panel, wxID_ANY, "My Checkbox", wxPoint(550, 55), wxDefaultSize, wxCHK_3STATE | wxCHK_ALLOW_3RD_STATE_FOR_USER); // pipe allows us to define multiple styles
+	CreateStatusBar();
+}
 
-	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, "Static text, not editable", 
-		wxPoint(0, 155), wxSize(400, -1), wxALIGN_CENTRE_HORIZONTAL);
-	staticText->SetBackgroundColour(*wxLIGHT_GREY);
+void MainFrame::OnButtonClicked(wxCommandEvent& evt) {
+	wxLogStatus("Clicked!!!");
+}
 
-	wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, "editable text", 
-		wxPoint(500, 145), wxSize(200, -1), wxTE_PASSWORD);
+void MainFrame::OnSliderChanged(wxCommandEvent& evt) {
+	wxString str = wxString::Format("Slider value: %d", evt.GetInt());
+	wxLogStatus(str);
+}
 
-	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1), wxSL_VALUE_LABEL);
-
-	wxGauge* progressBar = new wxGauge(panel, wxID_ANY, 100, wxPoint(590, 205), wxSize(-1, 125), wxGA_VERTICAL);
-	progressBar->SetValue(50);
-
-
-	wxArrayString choices;
-	choices.Add("Item B");
-	choices.Add("Item A");
-	choices.Add("Item C");
-
-	wxChoice* choice = new wxChoice(panel, wxID_ANY, wxPoint(150, 375), wxSize(100, -1), choices, wxCB_SORT);
-	choice->Select(0);
-
-
-	wxSpinCtrl* spinCtrl = new wxSpinCtrl(panel, wxID_ANY, "", wxPoint(550, 375), wxSize(100, -1), wxSP_WRAP); //wrap = loop increment/decrement
-
-	wxListBox* listBox = new wxListBox(panel, wxID_ANY, wxPoint(150, 475), wxSize(100, -1), choices, wxLB_MULTIPLE); //choices updatable during runtime
-
-	//wxRadioBox* radioBox = new wxRadioBox(panel, wxID_ANY, "radioBox", wxPoint(555, 450), wxDefaultSize, choices, 1);
-	wxRadioBox* radioBox = new wxRadioBox(panel, wxID_ANY, "radioBox", 
-		wxPoint(555, 450), wxDefaultSize, choices, 3, wxRA_SPECIFY_ROWS);
-
-	//wxWidgets automatically deallocate memory for these controls!
+void MainFrame::OnTextChanged(wxCommandEvent& evt) {
+	wxString str = wxString::Format("Text: %s", evt.GetString());
+	wxLogStatus(str);
 }
